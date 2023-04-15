a9416523ab78f3d707cf80fe6be3c3777d77263b
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-04-15 16:08:43 -0500

    Changed design/working on 5-day weather icons

---
+++ src/WeatherIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WeatherIcon(props) {
+
+  return (
+    <img
+      src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.code}.png`}
+      alt={props.alt}
+      size={props.size}
+    />
+  );
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,6 +19,7 @@ export default function WeatherInfo(props) {
               src={props.data.iconUrl}
               alt="{weatherData.description}"
               className="float-left"
+              size={52}
             />
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
