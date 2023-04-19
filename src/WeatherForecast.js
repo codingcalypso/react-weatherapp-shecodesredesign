04f5634b366a0040c636c007d496a58a431368ad
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-04-19 13:42:20 -0500

    Fixed deployment error - change min/max to full words/edit component icon link/change current in API to forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,11 +8,11 @@ export default function WeatherForecast(props) {
   let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+  console.log({ forecast });
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -25,9 +25,9 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "37a2a46b333cct4ffebf207fd51o0167";
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apiURL = `https://api.shecodes.io/weather/v1/current?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+    let latitude = props.coordinates.latitude;
+    let longitude = props.coordinates.longitude;
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiURL).then(handleResponse);
 
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,12 +3,12 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.temp.max);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.data.temp.min);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
   function day() {
@@ -22,7 +22,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} />
+      <WeatherIcon code={props.data.condition.icon} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
