25f5999233b80eea7eb40bfd3eb96d6dc7446dde
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-04-21 16:21:58 -0500

    Made some CSS/design changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,15 +6,9 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="d-flex">
+          <div className="d-flex weather-img-section">
             <img
               src={props.data.iconUrl}
               alt="{weatherData.description}"
@@ -27,6 +21,10 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
